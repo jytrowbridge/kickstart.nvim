d4ab6c430d15481a385d314504026cd202ae938a
Jack Trowbridge <jytrowbridge@gmail.com>
2025-09-24 11:33:14 -0400

    extract lazy plugins

--- lua/lazy-init.lua
+++ lua/lazy-init.lua
@@ -12,3 +12,16 @@ end
 ---@type vim.Option
 local rtp = vim.opt.rtp
 rtp:prepend(lazypath)
+
+-- [[ Configure and install plugins ]]
+--
+--  To check the current status of your plugins, run
+--    :Lazy
+--
+--  You can press `?` in this menu for help. Use `:q` to close the window
+--
+--  To update plugins you can run
+--    :Lazy update
+--
+-- NOTE: Here is where you install your plugins.
+require('lazy').setup 'plugins'
